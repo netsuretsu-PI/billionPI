d0945d9e91b21fb2cad3b8c39fa9777ded979927
Mojashi <akihiro1001@gmail.com>
2021-04-23 01:10:20 +0900

    implement interfaces

--- src/longVector/longVector.hpp
+++ src/longVector/longVector.hpp
@@ -5,9 +5,19 @@ using ull = unsigned long long int;
 
 template <class T>
 class LongVector {
-    std::vector<T> ar;
-    ull _size;
+    static const ull PAGE_SIZE = 1; //MB
+    static const ull CACHE_SIZE = 10; //pages
+    ull size;
 
 public:
+    LongVector(ull size);
     ull size();
+    //return the size after push_back
+    ull push_back(const T& v);
+    ull pop_back();
+    
+    T operator[](ull idx);
+    T resize(ull size);
+
+    ~LongVector(); //release resources (delete files)
 };
